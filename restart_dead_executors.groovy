dd75d44a59aed33a0d8fd9c9987e0b018379ebd7
Vlad Kuziura <vlad.kuziura@vkuziura.local>
2018-01-18 09:19:07 -0800

    Commented out first line in restart_dead_executors.groovy

--- restart_dead_executors.groovy
+++ restart_dead_executors.groovy
@@ -1,4 +1,4 @@
-# Restart all dead executros on Jenkins (use from Script Console)
+// Restart all dead executros on Jenkins (use from Script Console)
 def deadExecutors = Jenkins.instance.computers.collect { c ->
   c.executors.findAll { !it.isActive() }
 }.flatten()
