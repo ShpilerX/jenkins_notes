2b41a9d29dea9787134a241da01c835911ce6241
Vlad Kuziura <vlad.kuziura@vkuziura.local>
2018-01-18 09:17:26 -0800

    Minor adjustment to README

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Some scripts found here and there to keep in one place
+Some Jenkins related groovy scripts found here and there to keep in one place.
--- restart_dead_executors.groovy
+++ restart_dead_executors.groovy
@@ -1,4 +1,5 @@
-def deadExecutors = Jenkins.instance.computers.collect { c -> 
+# Restart all dead executros on Jenkins (use from Script Console)
+def deadExecutors = Jenkins.instance.computers.collect { c ->
   c.executors.findAll { !it.isActive() }
 }.flatten()
 deadExecutors.each { it.doYank() }
